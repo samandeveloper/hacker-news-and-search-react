008b4c8f45ba62aa7894bf7bdbc345f7283fd320
Saman M. Mohaghegh <sn.mohaghegh@gmail.com>
2022-12-09 11:45:22 -0700

    Updates

---
+++ precache-manifest.b206ff53e9c2f6f64c941602f91669df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0443e4ec2d3dc1acf0d62e1c49b27c1d",
+    "url": "/hacker-news-and-search-react/index.html"
+  },
+  {
+    "revision": "694144652cc9b3096d73",
+    "url": "/hacker-news-and-search-react/static/css/main.35784568.chunk.css"
+  },
+  {
+    "revision": "2829f658d05344f4543e",
+    "url": "/hacker-news-and-search-react/static/js/2.4904b4fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hacker-news-and-search-react/static/js/2.4904b4fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "694144652cc9b3096d73",
+    "url": "/hacker-news-and-search-react/static/js/main.a56e3f56.chunk.js"
+  },
+  {
+    "revision": "f47f3175c1eeaeb0c077",
+    "url": "/hacker-news-and-search-react/static/js/runtime-main.47a710cf.js"
+  }
+]);
